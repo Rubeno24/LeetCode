a0fdc1f2159b3fa0703197d3a2bcefebc9c00ac2
unknown <Rubeno24>
2023-11-21 15:47:57 -0800

    Leet code 28

--- LeetCode/src/App.java
+++ LeetCode/src/App.java
@@ -6,13 +6,9 @@ import java.util.Set;
 public class App {
     public static void main(String[] args) throws Exception {
 
-        int[] intArray = new int[] {4,3,2,1};
         
-       // System.out.println(Arrays.toString((sort(intArray))));
 
-        LeetCode58 x = new LeetCode58();
-       
-        System.out.println( x.lengthOfLastWord("   fly me   to   the moon  "));
+        
         
     }
 
