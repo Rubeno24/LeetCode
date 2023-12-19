797e45f7420c13d33b6469edd3b27217ec138063
Ruben Ortega <ruben000_2@live.com>
2023-12-19 13:04:34 -0800

    finshed LeetCode 11

---
+++ LeetCode/src/LeetCode11.java
@@ -0,0 +1,22 @@
+import oracle.net.aso.f;
+import oracle.net.aso.h;
+
+public class LeetCode11 {
+    public int maxArea(int[] height) {
+        int first=0;
+        int last= height.length-1;
+        int maxArea=0;
+        
+        while(first<last){
+            if(height[first]<height[last]){
+                maxArea=Math.max(maxArea, height[first]*(last-first));
+                first++;
+            }
+            else{
+                maxArea=Math.max(maxArea, height[last]*(last-first));
+                last--;
+            }
+        }
+        return maxArea;
+    }
+}
---
+++ LeetCode/src/driver.java
@@ -0,0 +1,20 @@
+import java.sql.*;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+class Driver {
+    static Statement st;
+    static Connection con;
+    static Scanner scnr = new Scanner(System.in);
+
+    public static void main(String[] args) throws Exception {
+        LeetCode11 x = new LeetCode11();
+        int arr []  = new int[]{1,8,6,2,5,4,8,3,7};
+        System.out.println(x.maxArea(arr));
+        x.maxArea(arr);
+        
+
+        
+    }
+
+}
