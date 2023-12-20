76bd135a2c104da1dafaa34e0bb65b9b75b9c1bb
Ruben Ortega <ruben000_2@live.com>
2023-12-20 09:49:41 -0800

    Commented LeetCode 169

--- LeetCode/src/Leetcode169.java
+++ LeetCode/src/Leetcode169.java
@@ -1,59 +1,49 @@
-import java.util.Arrays;
 import java.util.HashMap;
 
 public class Leetcode169 {
     public int majorityElement(int[] nums) {
-        //sorts the array
-        mergeSort(nums);
+        /*
+         * simpler solution
+         * 
+         * Arrays.sort(nums);
+         * 
+         * if (nums.length==1 || nums.length==2){
+         * return nums[0];
+         * }
+         * return nums[nums.length/2];
+         * }
+         */
 
-        if (nums.length == 1 || nums.length == 2) {
-            return nums[0];
+        int maxCount = 0;
+        int majorityElement = 0;
+        // hashmap with key value pair as 2 ints
+        HashMap<Integer, Integer> map = new HashMap<>();
+        // for loop to store every element in the array into our hashmap
+        for (int key : nums) {
+            // adds the key to the hashmpa and uses the getOrDefault method to get the value
+            // associated
+            // with the key, if no value is there then the default value is 0.
+            // if the value is there then we increment it by 1
+            map.put(key, map.getOrDefault(key, 0) + 1);
         }
-        //we assume the number that appears the most is in the middle so we return that
-        return nums[nums.length / 2];
-    }
-    
-    public static void mergeSort(int[] array) {
-        if (array.length > 1) {
-            // Find the middle of the array
-            int mid = array.length / 2;
 
-            // Split the array into two halves
-            int[] left = new int[mid];
-            System.arraycopy(array, 0, left, 0, mid);
+        // using a for loop to itterate through the hashmap
+        for (int key : map.keySet()) {
+            // if statement to check if value is greater then maxCount
+            if (map.get(key) > maxCount) {
+                // it is we assign max count with that value
+                maxCount = map.get(key);
+                // majorityElement will be set equal to the key which is the number and the
+                // value is
+                // how many times is occurs
+                majorityElement = key;
 
-            int[] right = new int[array.length - mid];
-            System.arraycopy(array, mid, right, 0, array.length - mid);
-
-            // Recursively sort both halves
-            mergeSort(left);
-            mergeSort(right);
-
-            // Merge the sorted halves
-            merge(array, left, right);
-        }
-    }
-
-    private static void merge(int[] array, int[] left, int[] right) {
-        int i = 0, j = 0, k = 0;
-
-        // Merge the two sorted arrays into a single sorted array
-        while (i < left.length && j < right.length) {
-            if (left[i] <= right[j]) {
-                array[k++] = left[i++];
-            } else {
-                array[k++] = right[j++];
             }
-        }
 
-        // Copy the remaining elements of left[], if any
-        while (i < left.length) {
-            array[k++] = left[i++];
         }
+        // we return the key which is the number that occurs the most
+        return majorityElement;
 
-        // Copy the remaining elements of right[], if any
-        while (j < right.length) {
-            array[k++] = right[j++];
-        }
     }
+
 }
