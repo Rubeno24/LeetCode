3f77073af320a49960a8a955cb70b07f35a53369
Ruben Ortega <ruben000_2@live.com>
2024-01-02 14:06:19 -0800

    Leet Code Pow(x,n)

--- LeetCode/src/LeetCode50.java
+++ LeetCode/src/LeetCode50.java
@@ -1,24 +1,30 @@
 public class LeetCode50 {
     public double myPow(double x, int n) {
-        double answer =1;
-        if(n < 0){
+        // varible will be retuned and be x^n
+        double answer = 1;
+        // check if n less than 0
+        // if so make n postive and set x = to is reciprical so x=1/x
+        if (n < 0) {
             n = -n;
             x = 1 / x;
         }
 
-        while(n != 0){
-            if((n & 1) != 0){
+        // keep looping unitl n=0
+        while (n != 0) {
+            // if the last significant bit of n =1 go into if loop
+            if ((n & 1) != 0) {
                 answer *= x;
-            } 
-                
+            }
+            // reguardless of least significant bit multply x=x*x
             x *= x;
+            // shifts n over to the right by one
+            // Example n = 011
+            // Shifted n= 01
             n >>= 1;
-            
+            // loop ends when n has been shifted over enought that it is 0
         }
-        
+        // return answer which is x^n
         return answer;
     }
-        
 
-    
 }
