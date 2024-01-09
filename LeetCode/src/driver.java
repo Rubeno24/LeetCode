c357037906e70f38b5b0c164b127dc629d759568
Ruben Ortega <ruben000_2@live.com>
2024-01-08 16:41:29 -0800

    Worked on quicksort will finish tommrow

--- LeetCode/src/driver.java
+++ LeetCode/src/driver.java
@@ -13,13 +13,21 @@ class Driver {
     static Scanner scnr = new Scanner(System.in);
 
     public static void main(String[] args) throws Exception {
-        int[] arr = new int[100];
+        int[] arr = new int[1000];
+        int[] arr1 = new int[] { 1, 8, 3, 9, 4, 5, 7 };
 
         Random rand = new Random();
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < arr.length; i++) {
             arr[i] = rand.nextInt(100) + 1;
 
         }
+        System.out.println("Before :");
+        System.err.println(Arrays.toString(arr1));
 
+        SortingAlgos quickSort = new SortingAlgos();
+        quickSort.quickSort(arr1);
+
+        System.out.println("After :");
+        System.err.println(Arrays.toString(arr1));
     }
 }
