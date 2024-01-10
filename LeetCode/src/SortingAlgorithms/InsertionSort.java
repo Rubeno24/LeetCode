e31b75bc0ac90f879b3e17872c5a9f512209887c
Ruben Ortega <ruben000_2@live.com>
2024-01-09 16:30:19 -0800

    Insertion Sort

--- LeetCode/src/SortingAlgorithms/InsertionSort.java
+++ LeetCode/src/SortingAlgorithms/InsertionSort.java
@@ -9,19 +9,20 @@ public class InsertionSort {
         for (int i = 1; i < arr.length; i++) {
             // assigns the current value of the array at i the the current val variable
             currentValue = arr[i];
-            // defining a variable j that is one index behind the array[i]
+            // defining a variable j that is one index behind the array[i]jjj
             int j = i - 1;
             // while will keep going untill j is less than or equal to zero and the array[j]
             // is greater then the current value
             while (j >= 0 && arr[j] > currentValue) {
-                // once that is the case we are going to store arr[j] inside of array[j+1] since
+                // once that is the case we are going to store arr[j] inside of array[j+1]
+                // sincekkjjijjkk
                 // its greater than the current value. This puts array[j] in the correct order
                 arr[j + 1] = arr[j];
                 // decrementing j keeps the j pointer going to the end of the array
                 j--;
             }
-            // we put the current which is the smallest number at j+1 since j=i-1 so that
-            // would be -1 so we add 1 to j to make it 0 the start of the array.
+            // we put the currenVal inside arr[j+1] since we decremented j-- so that would
+            // be like storing it inside arr[j] to flip the 2 numbers into the correct spot
             arr[j + 1] = currentValue;
         }
     }
