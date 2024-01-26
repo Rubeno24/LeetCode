436e296cf62f1fffed240e9bb477c2358a192bd3
Ruben Ortega <ruben000_2@live.com>
2024-01-25 17:34:29 -0800

    Worked more on leet code 643

--- LeetCode/src/LeetCode643.java
+++ LeetCode/src/LeetCode643.java
@@ -2,11 +2,18 @@ public class LeetCode643 {
     public double findMaxAverage(int[] nums, int k) {
         int left = 0;
         int sum = 0;
-        int right = 0;
-        for (int i = right; i < nums.length; i++) {
+        int maxSum = 0;
+        for (int right = 0; right < nums.length; right++) {
+            sum += nums[right];
 
+            if (right - left + 1 >= k) {
+                maxSum = Math.max(sum, maxSum);
+                sum -= nums[left];
+                left++;
+            }
         }
+        maxSum = Math.max(sum, maxSum);
 
-        return -1;
+        return (double) maxSum / k;
     }
 }
