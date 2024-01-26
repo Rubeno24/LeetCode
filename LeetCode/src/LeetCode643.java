7fe2ce7bbcc38ea188b6a20eec56ce05574bd3e0
Ruben Ortega <ruben000_2@live.com>
2024-01-25 20:13:59 -0800

    Added Leetcode 1732 for tom

--- LeetCode/src/LeetCode643.java
+++ LeetCode/src/LeetCode643.java
@@ -6,13 +6,12 @@ public class LeetCode643 {
         for (int right = 0; right < nums.length; right++) {
             sum += nums[right];
 
-            if (right - left + 1 >= k) {
+            if (right >= k - 1) {
                 maxSum = Math.max(sum, maxSum);
                 sum -= nums[left];
                 left++;
             }
         }
-        maxSum = Math.max(sum, maxSum);
 
         return (double) maxSum / k;
     }
