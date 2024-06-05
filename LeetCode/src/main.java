75a2daa3b7c4545a29c4d87f4250013933de8dda
Ruben <ruben000_2@live.com>
2024-06-04 18:57:29 -0700

    Worked on binary search algo

--- LeetCode/src/main.java
+++ LeetCode/src/main.java
@@ -1,15 +1,13 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import SortingAlgorithms.*;
 
 class main{
     public static void main(String[] args) throws Exception {
-        LeetCode2215 x = new LeetCode2215();
-        int[] list1 = new int[]{1,2,3,3};
-        int[] list2 = new int[]{1,1,2,2 };
-
-
-        ArrayList done =(ArrayList) x.findDifference(list1, list2);
-        System.out.println(done.toString());
+        BinarySearch binarySearch = new BinarySearch();
+        int[] array = new int[]{1,2,3,4,5,6,7,8,9,10};
+        System.out.println( binarySearch.Binary_Search(array, 3));
+        
     
         }
 }
