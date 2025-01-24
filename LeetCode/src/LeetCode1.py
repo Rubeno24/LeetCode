7eb4ec24300102de9f735ee82091d5d403c35d43
Ruben Ortega <ruben000_2@live.com>
2025-01-24 15:57:37 -0800

    redid Leetcode 1

--- LeetCode/src/LeetCode1.py
+++ LeetCode/src/LeetCode1.py
@@ -1,26 +1,21 @@
+
 from typing import List
 
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # Initialize an empty hashmap (dictionary)
-        hashMap = {}
 
-        # Iterate over each number and its index in the nums list
-        for i, number in enumerate(nums):
-            # Calculate the difference needed to reach the target sum
-            difference = target - number
-    
-            # Check if the difference exists in the hashmap
-            if difference in hashMap:
-                # If found, return the indices that sum up to the target
-                return [hashMap[difference], i]
-    
-            # Store the current number and its index in the hashmap
-            hashMap[number] = i
+        hashmap = {}
+        
+        for x in range(len(nums)):
+            targetNumber = target - nums[x]
+            if targetNumber not in hashmap:
+                hashmap[nums[x]] = x
+            else: 
+                return [hashmap[targetNumber] ,x ]
+            
 
 
-
-x = Solution()
 nums = [2,7,11,15]
-print(x.twoSum(nums,9))#0 1
+x = Solution().twoSum(nums,9)
+print(x)
