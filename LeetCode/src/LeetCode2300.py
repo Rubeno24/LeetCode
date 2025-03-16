9d3e8145bc351c262f1f085baf2526a337793f03
Ruben Ortega <ruben.d.ortega@medtronic.com>
2025-03-15 20:51:07 -0700

    LeetCode2300

--- LeetCode/src/LeetCode2300.py
+++ LeetCode/src/LeetCode2300.py
@@ -4,28 +4,30 @@ from typing import List
 class Solution:
     def successfulPairs(self, spells: List[int], potions: List[int], success: int) -> List[int]:
         potions.sort()
-        results = []
+        result = []
+
         for x in spells:
             l = 0
-            r = len(potions) - 1
-            inx = len(potions)
-
+            r =len(potions)-1
             while l <= r:
-                middle = (l+r)//2
-
-                if x * potions[middle] >= success:
-                    r = middle - 1
-                    inx = middle
+                m = (l+r) //2
+                if x * potions[m] >= success:
+                    r = m-1
                 else:
-                    l = middle + 1
-            results.append(len(potions) - inx)
-        return results
+                    l=m+1
+            result.append(len(potions) - l)
+        return result
+
+
 
 spells = [5,1,3]
 potions = [1,2,3,4,5]
 success = 7
-
-
-
+# 1 2 3 4 5
+#       l r 
 x = Solution().successfulPairs(spells,potions,success)
+#answer [4,0,3]
+
 print(x)
+
+print("meem")
--- LeetCode/src/LeetCode374.py
+++ LeetCode/src/LeetCode374.py
@@ -7,23 +7,8 @@
 
 class Solution:
     def guessNumber(self, n: int) -> int:
-        #use binary search to see what number a user picks from 1 to n
-        # we start at 1 and go to n which will be the number the users picks. So it could be 1 to 10 where 10 is n
-        start = 1
-        end = n 
-        # we want to keep going until start is less then and equal to end. Once start is bigger it means the 
-        # number was not found and we return -1
-        while(start<=end):
-            # we define the middle number in the while loop since its gonna be adjusted everytime untill we find the number
-            middle = (start+end)//2
-            # We pass in middle to the api and if it returns 0 the number was found and we return0
-            if guess(middle) == 0:
-                return middle
-            # We pass in middle to the api and if it returns -1 the number they guessed is higher then the middle 
-            # number so we get rid of all the bigger numbers
-            elif guess(middle) == -1:
-               end=middle-1
-            # We pass in middle to the api and if it returns 1 the number they guessed is lower so we get rid of the smaller number
-            elif guess(middle) ==1:
-              start=middle+1
-        return -1
+        pass
+
+
+
+
